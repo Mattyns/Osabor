1d944281cb57dbe97e748b7e095e305e7b42486d
Matheus Everton <81mme.matheus@gmail.com>
2024-09-06 17:42:29 -0300

    Initial commit

---
+++ script/receita.js
@@ -0,0 +1,9 @@
+function cafe() {
+    var cafe = document.getElementById('cafe').style.display = 'block'
+    
+}
+
+function cafe2() {
+    var cafe = document.getElementById('cafe').style.display = 'none'
+    
+}
