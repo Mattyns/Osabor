0db4fec01a8e25a574d84849edecd15bd154e581
Matheus Everton <81mme.matheus@gmail.com>
2024-09-07 17:33:47 -0300

    07/09/24

--- script/receita.js
+++ script/receita.js
@@ -7,3 +7,23 @@ function cafe2() {
     var cafe = document.getElementById('cafe').style.display = 'none'
     
 }
+
+function alm() {
+    var alm = document.getElementById('alm').style.display = 'block'
+    
+}
+
+function alm2() {
+    var alm = document.getElementById('alm').style.display = 'none'
+    
+}
+
+function jantar() {
+    var jantar = document.getElementById('jantar').style.display = 'block'
+    
+}
+
+function jantar2() {
+    var jantar = document.getElementById('jantar').style.display = 'none'
+    
+}
